4abce162d6dc762f89ac0939cde8c9df203fa58d
Hasib <hasibhosen7612@gmail.com>
2023-06-10 15:42:05 +0600

    feat/auth-check & admin-check

---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+exports.requireSignIn = (req, res, next) => {
+  const token = req.headers.authorization;
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ status: "Unauthorized", data: error });
+  }
+};
+
+exports.isAdmin = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id).exec();
+    if (user.role !== 1) {
+      res.status(401).json({ status: "Unauthorized" });
+    } else {
+      next();
+    }
+    next();
+  } catch (error) {
+    res.status(401).json({ status: "Unauthorized", data: "error" });
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 const { register, login } = require("../controllers/auth");
+const { requireSignIn, isAdmin } = require("../middlewares/auth");
 
 router.post("/register", register);
 router.post("/login", login);
+
+router.get("/auth-check", requireSignIn, (req, res) => {
+  res.json({ status: "success" });
+});
+router.get("/admin-check", requireSignIn, isAdmin, (req, res) => {
+  res.json({ status: "success" });
+});
 
 module.exports = router;
